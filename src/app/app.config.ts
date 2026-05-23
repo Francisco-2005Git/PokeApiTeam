35163d2c3f9db069b07ea48f11f0ded11529a1b0
yorch-666s <l23220002@rioverde.tecnm.mx>
2026-05-23 02:26:52 -0600

    carpeta de components y services

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,26 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { provideHttpClient} from '@angular/common/http';
+import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list';
+import { PokemonService } from './services/pokemon';
+
+// @NgModule({
+//   declarations: [ PokemonDetailComponent,PokemonListComponent,PokemonService ],
+//   imports: [
+//     provideBrowserGlobalErrorListeners,
+//     provideHttpClient // ¡Agrega esta línea aquí!
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient()
   ]
 };
